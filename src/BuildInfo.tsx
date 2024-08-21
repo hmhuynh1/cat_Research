f2c0e73d9d536d9668edd3f8edc1efdf51b4b87b
hmhuynh1 <hmhuynh1@dmacc.edu>
2024-08-20 23:01:01 -0500

    clean up

--- src/BuildInfo.tsx
+++ src/BuildInfo.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 export default function BuildInfo() {
     return (<Box display={'flex'} flexDir={'row'} gap={10}>
-        <span>BuildTime: {import.meta.env.VITE_BUILD_TIME ?? 'DEV_HOT_RELOAD'}</span>
-        <span>BuildID: {import.meta.env.VITE_BUILD_ID ?? 'DEV_HOT_RELOAD'}</span>
+        {/* <span>BuildTime: {import.meta.env.VITE_BUILD_TIME ?? 'DEV_HOT_RELOAD'}</span>
+        <span>BuildID: {import.meta.env.VITE_BUILD_ID ?? 'DEV_HOT_RELOAD'}</span> */}
     </Box>);
 }
