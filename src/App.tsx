d79b3393ad0e057291dd83d77413d51f7de50ee2
hmhuynh1 <hmhuynh1@dmacc.edu>
2024-08-24 16:35:14 -0500

    add code

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
                 flexDirection="column"
                 justifyContent="center"
                 alignItems="flex-start"
-                bg="rgba(230, 240, 237, 20)"
+                bg="rgba(230, 240, 237, 0.50)"
                 minHeight="60vh"
                 position="relative"
                 zIndex="100"
